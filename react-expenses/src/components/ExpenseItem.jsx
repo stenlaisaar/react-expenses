f1a9df248a686c58be53bd3a01ffd5c7b3ac12fc
unknown <sten.laisaar@voco.ee>
2026-05-26 17:14:32 +0300

    ÜL 3 Card komponendi tegemine

--- react-expenses/src/components/ExpenseItem.jsx
+++ react-expenses/src/components/ExpenseItem.jsx
@@ -1,15 +1,16 @@
+import Card from './UI/Card'
 import ExpenseDate from './ExpenseDate'
 import './UI/ExpenseItem.css'
 
 const ExpenseItem = (props) => {
     return (
-       <div className='expense-item'>
+       <Card className='expense-item'>
             <ExpenseDate date={props.data.date}/>
             <div className='expense-item__description'>
                 <h2>{props.data.title}</h2>
                 <div className='expense-item__price'>{props.data.price}</div>
             </div>
-        </div>
+        </Card>
     )
 }
 
